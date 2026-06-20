92349b4993d0b5e2b2998692d873af977be40a45
NongThiThaoNguyen <nguyenntttb01430@gmail.com>
2026-06-20 10:10:14 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DuAnTotNghiep.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
